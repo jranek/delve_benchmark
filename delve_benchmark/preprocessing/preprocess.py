1006525a6cf82329368f5effd466cbefcb55d4ca
jranek <jolene@Jolenes-MacBook-Pro.local>
2023-11-17 11:55:53 -0800

    add DE data preprocessing

--- delve_benchmark/preprocessing/preprocess.py
+++ delve_benchmark/preprocessing/preprocess.py
@@ -3,6 +3,8 @@ import numpy as np
 import scanpy as sc 
 import anndata
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import scvelo as scv
+import delve_benchmark
 
 def standardize(x: np.ndarray = None):
     """Standardizes data by removing the mean and scaling to unit variance.
@@ -150,7 +152,7 @@ def create_rpe_adata(df = None,
     ----------
 
     Returns
-    adata: anndata.AnnData (default = None)
+    adata: anndata.AnnData
         Anndata object containing 4i data and all necessary appended observations
     ----------
     """
@@ -275,7 +277,7 @@ def create_pdac_adata(df = None,
     ----------
 
     Returns
-    adata: anndata.AnnData (default = None)
+    adata: anndata.AnnData 
         Anndata object containing 4i data and all necessary appended observations
     ----------
     """
@@ -298,4 +300,152 @@ def create_pdac_adata(df = None,
 
     adata.layers['raw'] = df_cleaned.copy()
 
+    return adata
+
+def filter_DE(adata,
+            min_genes = 650,
+            max_genes = 7200,
+            percent_mito = 0.2,
+            min_shared_counts = 5,
+            min_counts = 1500,
+            max_counts = 90000):
+    """Performs QC filtering on the definitive endoderm dataset by filtering cells and genes according to appropriate cutoffs
+
+    Parameters    
+    adata: anndata.AnnData
+        anndata object containing the single-cell RNA sequencing data
+    min_genes: int (default = 650)
+        minumum number of genes expressed within a cell
+    max_genes: int (default = 7200)
+        maximum number of genes expressed within a cell
+    percent_mito: float (default = 0.2)
+        proportion of mitochondrial genes expressed
+    min_shared_counts: int (default = 5)
+        minumum number of unspliced and spliced molecular counts
+    min_counts: int (default = 1500)
+        minumum number of gene counts 
+    max_counts: int (default = 90000)
+        maximum number of gene counts
+    ----------
+
+    Returns
+    adata: anndata.AnnData
+        Filtered anndata object 
+    ----------
+    """
+    sc.pp.filter_cells(adata, min_genes = min_genes)
+    sc.pp.filter_cells(adata, max_genes = max_genes)
+    adata = adata[adata.obs['percent_mito'] <= percent_mito]
+    scv.pp.filter_genes(adata, min_shared_counts = min_shared_counts)
+    sc.pp.filter_cells(adata, min_counts = min_counts)
+    sc.pp.filter_cells(adata, max_counts = max_counts)
+
+    return adata
+
+def demultiplex(adata, mat, barcodes, gene_names, numBarcodes = 3):
+    ####Performs de-multiplexing by linking the maximum median normalized condition-specific barcode to each cell
+    fulldf = pd.DataFrame(mat.todense(), columns = barcodes, index = gene_names)
+    bcddf = fulldf.iloc[-numBarcodes:len(fulldf), :]
+    median_bcd = bcddf.median(axis = 1)
+    mednorm_bcd = bcddf.div(median_bcd, axis = 'index') #median normalized data
+    bcd_df = mednorm_bcd.copy() #converts downstream analysis into looking at median normalized data 
+    barcode_groups = bcd_df.idxmax()
+
+    barcodeReind = sorted(range(len(adata.obs_names)), key=lambda k: adata.obs_names[k]) #gives index of alphabetical cell barcodes
+    Z = [x for _,x in sorted(zip(barcodeReind, barcode_groups))] #reoder feature barcodes by median normalized max
+    return Z
+    
+def preprocess_DE(adata,
+                mat,
+                barcodes,
+                gene_names,
+                numBarcodes = 3,
+                barcode_dict = {'FtrBarcode1': 'd0','FtrBarcode2': 'd1', 'FtrBarcode3': 'd2'},
+                min_genes = 650,
+                max_genes = 7200,
+                percent_mito = 0.2,
+                min_shared_counts = 5,
+                min_counts = 1500,
+                max_counts = 90000,
+                n_top_genes = 2000,
+                n_neighbors = 10,
+                n_pcs = 50,
+                groupby = 'day',
+                n_jobs = -1):
+    """Preprocesses DE data by:
+        1. QC filtering
+        2. de-mulitplexing
+        3. CPM normalization + log+1 transformation
+        4. HVG selection
+        5. RNA velocity estimation
+
+    Parameters    
+    adata: anndata.AnnData
+        anndata object containing the single-cell RNA sequencing data
+    mat, barcodes, gene_names:
+        output from 10X Genomics filtered_feature_bc_matrix 
+    numBarcodes: int (default = 3)
+        number of condition-specific oligo barcodes for demulitplexing
+    barcode_dict: dict 
+        dictionary mapping the feature barcode labels to condition-specific labels 
+    min_genes: int (default = 650)
+        minumum number of genes expressed within a cell
+    max_genes: int (default = 7200)
+        maximum number of genes expressed within a cell
+    percent_mito: float (default = 0.2)
+        proportion of mitochondrial genes expressed
+    min_shared_counts: int (default = 5)
+        minumum number of unspliced and spliced molecular counts
+    min_counts: int (default = 1500)
+        minumum number of gene counts 
+    max_counts: int (default = 90000)
+        maximum number of gene counts
+    n_top_genes: int (default = 2000)
+        number of highly variable genes 
+    n_neighbors: int (default = 10)
+        number of k-nearest neighbors
+    n_pcs: int (default = 50)
+        number of principle components
+    groupby: str (default = 'day')
+        labels key for RNA velocity differential kinetics correction
+    n_jobs: int (default = -1)
+        number of parallel processes 
+    ----------
+
+    Returns
+    adata: anndata.AnnData
+        preprocessed anndata object 
+    ----------
+    """
+    cell_barcodes = demultiplex(adata, mat, barcodes, gene_names, numBarcodes = numBarcodes)
+    adata.obs['barcode_groups'] = pd.Categorical(cell_barcodes)
+    adata.obs['day'] = pd.Series(adata.obs['barcode_groups']).map(barcode_dict)
+
+    adata.obs['n_counts'] = adata.X.sum(1)
+    adata.obs['n_genes'] = (adata.X > 0).sum(1)
+    mito_genes = adata.var_names.str.startswith('MT-')
+    adata.obs['percent_mito'] = np.sum(adata[:, mito_genes].X, axis = 1).A1 / np.sum(adata.X, axis = 1).A1
+
+    sc.pl.violin(adata, ['n_genes', 'n_counts', 'percent_mito'], jitter = 0.4, multi_panel = True)
+    sc.pl.scatter(adata, x='n_counts', y='percent_mito')
+    sc.pl.scatter(adata, x='n_counts', y='n_genes')
+
+    print(adata.shape)
+
+    adata = filter_DE(adata, min_genes = min_genes, max_genes = max_genes, percent_mito = percent_mito, min_shared_counts = min_shared_counts, min_counts = min_counts, max_counts = max_counts)
+
+    sc.pl.violin(adata, ['n_genes', 'n_counts', 'percent_mito'], jitter = 0.4, multi_panel = True)
+    sc.pl.scatter(adata, x='n_counts', y='percent_mito')
+    sc.pl.scatter(adata, x='n_counts', y='n_genes')
+
+    print(adata.shape)
+
+    adata.layers['raw'] = adata.X.copy()
+
+    sc.pp.normalize_total(adata, target_sum = 1e6)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, flavor = 'seurat', n_top_genes = n_top_genes, subset = True)
+
+    sc.pp.neighbors(adata, n_neighbors = n_neighbors)
+    adata = delve_benchmark.tl.perform_velocity_estimation(adata = adata, n_pcs = n_pcs, k = n_neighbors, var_names = 'all', mode = 'dynamical', groupby = groupby, likelihood_threshold = 0.001, n_jobs = n_jobs)
     return adata
